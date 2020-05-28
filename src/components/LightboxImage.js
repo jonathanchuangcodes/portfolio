a4f77db727f01a278636c7922f41a5262c757ad0
Jonathan Chuang <jchuangsa@gmail.com>
2020-05-27 18:03:49 -0700

    lightbox navigation styling added

--- src/components/LightboxImage.js
+++ src/components/LightboxImage.js
@@ -19,6 +19,42 @@ const LightboxImage = (props) => {
     setViewerIsOpen(false);
   };
 
+  const navButtonStyles = base => ({
+    ...base,
+    color: 'white',
+    width: '80px',
+    height: '80px',
+
+    '&:hover, &:active': {
+      backgroundColor: 'white',
+      boxShadow: '0 1px 3px rgba(0, 0, 0, 0.14)',
+      color: 'black',
+      opacity: 1,
+    },
+    '&:active': {
+      boxShadow: '0 1px 3px rgba(0, 0, 0, 0.14)',
+      transform: 'scale(0.96)',
+      color: 'black'
+    },
+  });
+
+  const closeButtonStyles = base => ({
+    ...base,
+    color: 'white',
+
+    '&:hover, &:active': {
+      backgroundColor: 'white',
+      boxShadow: '0 1px 3px rgba(0, 0, 0, 0.14)',
+      color: 'black',
+      opacity: 1,
+    },
+    '&:active': {
+      boxShadow: '0 1px 3px rgba(0, 0, 0, 0.14)',
+      transform: 'scale(0.96)',
+      color: 'black'
+    },
+  });
+
   const { images, direction, columns } = props;
   // TODO: find a better way to query for both the large and small image
   const data = useStaticQuery(graphql`
@@ -74,9 +110,18 @@ const LightboxImage = (props) => {
       <Gallery direction={direction} columns={columns} className="lightbox-gallery" photos={filteredPhotos}  onClick = {openLightbox} />
       <ModalGateway>
         {viewerIsOpen ? (
-          <Modal allowFullscreen={false} preventScroll={false} onClose ={closeLightbox}>
+          <Modal allowFullscreen={false} onClose ={closeLightbox}>
             <Carousel
               currentIndex = {currentImage}
+              styles={{
+                navigationPrev: navButtonStyles,
+                navigationNext: navButtonStyles,
+                headerClose: closeButtonStyles,
+                container: base => ({
+                  ...base,
+                  overflowY: 'auto',
+                })
+              }}
               views = {filteredPhotos.map(x => ({
                 ...x,
                 srcset: x.srcSet,
