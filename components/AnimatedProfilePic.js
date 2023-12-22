fa9019e3bf3db2245c5e7a9718d24abbbeb76541
jonathanchuangcodes <jchuangsa@gmail.com>
2023-12-22 14:11:58 -0500

    modify max width of home page profile pic

--- components/AnimatedProfilePic.js
+++ components/AnimatedProfilePic.js
@@ -16,7 +16,7 @@ export default function AnimatedProfilePic() {
         src={ProfilePicture}
         alt="profile picture"
         priority={true}
-        sizes="(max-width: 768px) 40vw, (max-width: 1200px) 45vw, 33vw"
+        sizes="(max-width: 768px) 35vw, (max-width: 1200px) 40vw, 30vw"
       />
     </animated.div>
   )
