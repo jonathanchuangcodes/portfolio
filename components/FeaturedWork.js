e07fc4f154fd7b6c4ceab6757b8857efa00b65ab
jonathanchuangcodes <jchuangsa@gmail.com>
2024-01-07 16:40:52 -0500

    add technology used, module in jsconfig

--- components/FeaturedWork.js
+++ components/FeaturedWork.js
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import Image from "next/image"
 import "../styles/sass/featuredWork.scss"
+import { GatsbyFill, NextjsFill, ReactFill, ReduxFill } from "akar-icons"
 
 export default function FeaturedWork({ work }) {
   return (
@@ -19,6 +20,24 @@ export default function FeaturedWork({ work }) {
             <p className="featured-work-role">
               {work.type + " | " + work.my_role}
             </p>
+            {work.icons && (
+              <div className="featured-work-tech">
+                {work.icons?.map((tech, index) => {
+                  switch (tech) {
+                    case "gatsby":
+                      return <GatsbyFill key={index} />
+                    case "nextjs":
+                      return <NextjsFill key={index} />
+                    case "react":
+                      return <ReactFill key={index} />
+                    case "redux":
+                      return <ReduxFill key={index} />
+                    default:
+                      return <p key={index}>{tech}</p>
+                  }
+                })}
+              </div>
+            )}
             <p className="featured-work-description">{work.description}</p>
           </div>
           <div className="featured-work-image-container">
